8503d354feb00b53abb7aa644f7e7485b10c474e
Michael Marvin <marsinmike@live.com>
2023-04-03 16:27:00 -0400

    final touches

--- src/Components/Lessons/Quote.tsx
+++ src/Components/Lessons/Quote.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Quote() {
     return (
             <div className="plato-quote" id="Teaching-philosophy">
--- src/Components/Performances.tsx
+++ src/Components/Performances.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Performances() {
     return (
         <div className="Performances">
